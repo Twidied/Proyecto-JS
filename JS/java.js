4b17be0e673d0b5074447f8702551e68503104a5
Diego Diaz <diegodiazcruz262007@gmail.com>
2025-09-02 12:54:36 -0500

    :rocket: Se agrego el API

---
+++ JS/java.js
@@ -0,0 +1,42 @@
+const apiUrl = "https://fakestoreapi.com/products";
+
+function mainCard(character) {
+    const { image, price } = character;
+    const container1 = document.querySelector(".container1");
+
+    // Imagen del producto
+    const characterImage = document.createElement("img");
+    characterImage.src = image;
+    characterImage.width =180;
+
+    // Precio del producto
+    const characterPrice = document.createElement("p");
+    characterPrice.textContent = `$${price}`;
+
+    // Contenedor de cada tarjeta
+    const card = document.createElement("div");
+    card.appendChild(characterImage);
+    card.appendChild(characterPrice);
+    card.style.backgroundColor = "grey";
+    card.style.padding = "50px";
+    card.style.margin = "5px";
+
+
+    
+    container1.appendChild(card);
+}
+
+async function getCharacters() {
+    try {
+        const response = await fetch(apiUrl);
+        const results = await response.json();
+
+        for (let i = 0; i < results.length; i++) {
+            mainCard(results[i]);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getCharacters();
