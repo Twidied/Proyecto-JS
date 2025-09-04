b19d1925ff2646e7fee86acc4fee1a5a6789a790
Diego Diaz <diegodiazcruz262007@gmail.com>
2025-09-04 05:45:41 -0500

    :sparkles: Se agrega Localstorage

---
+++ JS/storage.js
@@ -0,0 +1,12 @@
+
+function getCart() {
+    return JSON.parse(localStorage.getItem("cart")) || [];
+}
+
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function clearCart() {
+    localStorage.removeItem("cart");
+}
